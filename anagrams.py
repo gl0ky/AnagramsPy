0b92e47bb0ef2c5d770ad4f38874ba40e1994862
Claudio Garcia <cgarcia1502@hotmail.com>
2021-05-14 17:34:30 -0400

    Funciones de comparacion y ordenamiento agregadas

--- anagrams.py
+++ anagrams.py
@@ -1,1 +1,44 @@
-print("Hello World!")
+#!/bin/python3
+import sys
+import os
+
+def stringSorter(string):
+
+    string = string.strip(" ")
+    string = string.lower()
+    txt = sorted(string)
+    txt = ''.join(txt)
+    return txt
+
+def stringComparer(string_1, string_2):
+
+    if(string_1 == string_2):
+
+        return True
+    
+    elif(string_1 != string_2):
+
+        return False
+
+def anagrams(string_1, string_2):
+
+    if string_1 == string_2:
+
+        print ("Los strings son iguales...")
+    
+    else:
+
+        string_1_sorted = stringSorter(string_1)
+        string_1_sorted = stringSorter(string_1)
+
+
+def usage():
+
+    print(f"Usage: {sys.argv[0]} ejemplo string 1 ejemplo string 2")
+
+def main():
+
+    print(stringSorter("Claudio Garcia"))
+
+if __name__ == '__main__':
+    main();
